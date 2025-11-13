a7d47b46fc33ae564f0947b2b363e0a19de871c8
Utkarsh Jogdand <104009113+utkarsh5772@users.noreply.github.com>
2025-11-14 00:31:02 +0530

    Time: 3 ms (94.51%) | Memory: 57.4 MB (62.91%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let left = 0;
+    let maxlength = 0;
+    let charSet = new Set();
+
+    for(let right = 0; right < s.length; right++){
+        while(charSet.has(s[right])){
+            charSet.delete(s[left])
+            left++;
+        }
+        charSet.add(s[right]);
+        maxlength = Math.max(maxlength, right - left + 1);
+    }
+    
+    return maxlength;
+};
