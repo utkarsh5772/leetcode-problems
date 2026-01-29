0d2cc7b13c2ef59f92a8c3a31bd97cc61e237ece
Utkarsh Jogdand <104009113+utkarsh5772@users.noreply.github.com>
2026-01-29 14:37:35 +0530

    Time: 2 ms (37.00%) | Memory: 58.2 MB (51.47%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.js
@@ -0,0 +1,13 @@
+var transpose = function(matrix) {
+    const row = matrix.length;
+    const col = matrix[0].length;
+    const trans = Array.from({ length: col }, () => Array(row));
+
+    for (let i = 0; i < col; i++) {
+        for (let j = 0; j < row; j++) {
+            trans[i][j] = matrix[j][i];
+        }
+    }
+
+    return trans;
+};
