358c4051404a73dec97209d91c6153e488595fa8
Utkarsh Jogdand <104009113+utkarsh5772@users.noreply.github.com>
2026-02-05 13:36:48 +0530

    Time: 7 ms (31.95%) | Memory: 14.9 MB (13.91%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        counts = Counter(nums)
+        index = 0
+        result = []
+        non_repeated = [item for item, count in counts.items() if count == 1]
+        for x in range(0,len(non_repeated)):
+            index = 0
+            for num in nums:
+             if non_repeated[x] != num:
+                index ^= num
+            result.append(index)
+        return result         
